474649d96fd49716d461c3ace82943f885778eee
Suva0412 <162797270+Suva0412@users.noreply.github.com>
2024-03-09 13:31:53 +0300

    upload test

---
+++ Sakshi/src/com/Pelatro/Arjun/GitHub.java
@@ -0,0 +1,10 @@
+package com.Pelatro.Arjun;
+
+public class GitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is a test program to upload in github");
+	}
+
+}
